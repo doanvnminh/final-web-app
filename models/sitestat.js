1742e2d243e41f6afad4a6a86db3fc75a14bafd0
doanvnminh <doanvnminh160@gmail.com>
2026-01-30 16:27:41 +0700

    Rental Web Project

--- models/sitestat.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const statSchema = new mongoose.Schema({
-    name: { type: String, default: 'site_views' },
-    count: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('SiteStat', statSchema);
